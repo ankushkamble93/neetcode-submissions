dfa8e9c0a666f81eadc65cd05c618f23dc7163e1
Ankush Kamble <109985321+ankushkamble93@users.noreply.github.com>
2026-07-03 11:32:28 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_str = ""
+        for char in s:
+            if char.isalnum():
+                clean_str+=char.lower()
+        return clean_str == clean_str[::-1]
